357b6c3a3c013324202de961e54563dcfcbdc2ff
Megan Fago <meganlaynefago@gmail.com>
2018-11-20 15:40:43 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  squared_numbers = []
+    numbers.each do |number|
+    squared_numbers<<number**2
+  end
+return squared_numbers
 end
